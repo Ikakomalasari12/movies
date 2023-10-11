bcdc71236ea44bdc7dcb0ab06b9e065ce4b185b6
Ika Komalasari <komalaika229@gmail.com>
2023-10-11 16:20:10 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from http import client
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("mongodb+srv://test_ikakomalasari:sparta@test.nrkvr1l.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME =  os.environ.get("dbsparta")
 
 client = MongoClient('mongodb+srv://test_ikakomalasari:sparta@test.nrkvr1l.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
